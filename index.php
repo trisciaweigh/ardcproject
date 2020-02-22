6b1d9c569ddbc7427525bd85193dedee85a52c81
trisciaweigh <60768752+trisciaweigh@users.noreply.github.com>
2020-02-22 19:46:16 +0800

    change title

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet"> 
-		<title>ARDC</title>
+		<title>ARCHER REALTY AND DEVELOPMENT CORPORATION</title>
         
          <script type="text/javascript">
              function employeeTableDisplay(){
