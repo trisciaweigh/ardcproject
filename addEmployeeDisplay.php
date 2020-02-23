f044d69853d00fa69e8f2c5dc6ad620176c93a53
trisciaweigh <60768752+trisciaweigh@users.noreply.github.com>
2020-02-23 19:32:33 +0800

    change

--- addEmployeeDisplay.php
+++ addEmployeeDisplay.php
@@ -6,14 +6,14 @@ include 'connect.php';
 <html>
 <head>
     <meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<link rel="stylesheet" href="style.css" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet"> 
-		<title>ARDC Add Employee</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet"> 
+    <title>ARDC Add Employee</title>
 </head>
 <body>
     <div id="headerCompanyName">
@@ -28,7 +28,7 @@ include 'connect.php';
                 <label>Employee No.</label>
             </div> 
              <div class="addEmpInput">
-                 <input type="text" name="empno" id="empno" class="addEmpClass addEmpIndent" autocomplete="off" onchange="checkEmpNo(this.value)">
+                 <input type="text" name="empno" id="empno" class="addEmpClass addEmpIndent" autocomplete="off>
                  <span class="error" id="empError">Employee no. already exist!</span>
             </div> 
             
@@ -273,35 +273,17 @@ include 'connect.php';
         </form>
         
     </div>
-    
-    <script>        
-            function checkEmpNo(no){
-                var xml = new XMLHttpRequest();
-                    xml.onreadystatechange = function() {
-                        if (xml.readyState == 4 && xml.status == 200) {
-                            if(this.responseText=="existing"){
-                                document.getElementById("empno").style.borderBottom = "2px solid red";
-                                document.getElementById("empError").style.display = "inline-block";
-                            }else{                                
-                                document.getElementById("empno").style.borderBottom = "2px solid dimgray";
-                                document.getElementById("empError").style.display = "none";
-                            }
-                        }
-                    };
-                    xml.open("get","toValidateEmployeeNo.php?i=" + no, true);
-                    xml.send();
-                    return false;
-            }
 
+    <script>        
             $(document).ready(function(){
                 var validEmail = false;
 
 
 
 
-                $("input").focus(function(){
-                    $(this).css("border-bottom", "3px solid #0762f5");
-                });
+                // $("input").focus(function(){
+                //     $(this).css("border-bottom", "3px solid #0762f5");
+                // });
                 // $("input").blur(function(){
                 //     $(this).css("border-bottom", "2px solid dimgray");
                 // });
@@ -314,12 +296,12 @@ include 'connect.php';
                     },
                     function (data) {
                         if(data === "existing"){
-                            $(this).css("border-bottom", "2px solid red");
+                            $("#empno").css("border-bottom", "2px solid red");
                             validEmail = false;
                         }
                         else{
                             alert("ala");
-                            $(this).css("border-bottom", "2px solid red");
+                            $("#empno").css("border-bottom", "2px solid #00ff00");
                             validEmail = true;
                         }
 
@@ -330,5 +312,7 @@ include 'connect.php';
             });
      
     </script>
+    
+    
     </body>
 </html>
